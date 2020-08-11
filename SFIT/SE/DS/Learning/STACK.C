4be3dc7b568f224cd55600356c38ac5066dfdac0
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-08-11 18:54:45 +0530

    Stack V1

--- SFIT/SE/DS/Learning/STACK.C
+++ SFIT/SE/DS/Learning/STACK.C
@@ -1,3 +1,4 @@
+/* Stack implementation using Array*/
 #include<stdlib.h>
 #include<stdio.h>
 #define size 5
@@ -13,6 +14,29 @@ void push(int val)
 		stack[top]=val;
 	}
 }
+
+void pop()
+{
+
+	if(top==-1)
+		printf("\nStack Empty!!\n");
+	else
+	{
+		printf("\nElement removed=%d",stack[top]);
+		top--;
+	}
+}
+void peep()
+{
+	int val;
+	if(top==-1)
+		printf("\nStack Empty!!\n");
+	else
+	{
+		printf("Element at top=%d",stack[top]);
+
+	}
+}
 void display()
 {	int i;
 	if(top==-1)
@@ -20,33 +44,40 @@ void display()
 	else
 	{	printf("\nStack elements:\n");
 		for(i=top;i>=0;i--)
-				printf("%d\n",stack[i]);
-		
+		{
+		  printf("%d\n",stack[i]);
+		  }
+
 	}
 }
 
 void main()
-{	
-	int choice;int val;
-	 clrscr();
+{
+	int choice;
+	int val;
+	clrscr();
 	while(1)
 	{
-	printf("\n1. Push  2. Display  3. Exit\n Enter your  choice:");
+	printf("\n1. Push \n2. Pop  \n3. Peep  \n4. Display  \n5. Exit\n Enter your  choice:");
 	scanf("%d",&choice);
-	
+
 	switch(choice)
 	{
-		case 1:printf("Enter value: ");
-				   scanf("%d",&val);
-				   push(val);
-           break;
-		case 2: display();
-            break;
-		case 3: exit(0);
+		case 1:	printf("Enter value: ");
+			scanf("%d",&val);
+			push(val);
+			break;
+		case 2: pop();
+			break;
+		case 3: peep();
+			break;
+		case 4: display();
+			break;
+		case 5: exit(0);
 		default: printf("Enter valid choice!!\n");
-		
+
 	}
-         getch(); 
+	 getch();
 	}
-	
+
 }

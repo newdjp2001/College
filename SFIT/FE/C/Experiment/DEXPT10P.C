3bd7d3af2052f3f6cf51a0dbf0c63306b8b34f25
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-08-13 20:32:47 +0530

    Pattern 1

---
+++ SFIT/FE/C/Experiment/DEXPT10P.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i,j,k,b,c;
+ clrscr();
+ for(i=0;i<=4;i++)
+ {
+  b=4-i;
+  for(j=0;j<=b;j++)
+  {
+   printf("\t");
+		 }
+  c=(2*i)-1;
+  for(k=1;k<=c;k++)
+  {
+   if(i==4)
+   {
+    printf("*\t");
+		  }
+   else
+   {
+    if(k==1||k==(2*i)-1)
+   {
+    printf("*\t");
+		  }
+   else
+   {
+    printf("\t");
+		 }
+		  }
+		   }
+  printf("\n");
+   }
+ getch();
+ }

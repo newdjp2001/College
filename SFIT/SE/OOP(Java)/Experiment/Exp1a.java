dfe01d404f9c1e9d4abc4044ec2e402e2b0530a0
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-08-19 18:30:09 +0530

    Dollar to Rupee and Vice Versa (Static)

---
+++ SFIT/SE/OOP(Java)/Experiment/Exp1a.java
@@ -0,0 +1,21 @@
+class Exp1a
+{
+ public static void main(String A[])
+   {
+     int conv = 0;
+     float dollar=2f,rupee=74.86f;
+     if(conv==1)
+     {
+      rupee=dollar*74.86f;
+      System.out.println(dollar+" Dollars = "+rupee+" Rupees");
+       }
+      else
+      {
+       dollar=rupee/74.86f;
+       System.out.println(rupee+" Rupees = "+dollar+" Dollars");
+       }
+
+   }
+}
+     
+  

b83819014a7fc416d47cf900c8cd90f759c1ebe9
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-09-04 23:29:04 +0530

    Student V2

--- SFIT/SE/OOP(Java)/Learning/Student.java
+++ SFIT/SE/OOP(Java)/Learning/Student.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 class StudentInfo
  	{
@@ -18,9 +21,15 @@ class StudentInfo
  }	
 	public class Student 
 	{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter Roll no");
+		String str=br.readLine();
+		int rn = Integer.parseInt(str);
 		StudentInfo s1 = new StudentInfo();
-		s1.setData(14,70.5f);
+		
+		s1.setData(rn,70.5f);
 		s1.getData();
 	}
 

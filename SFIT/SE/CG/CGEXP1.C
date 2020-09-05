84b5d56e18a65d28ede9e73bcc47c8fe8ee99fce
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-09-05 13:00:57 +0530

    DDA

---
+++ SFIT/SE/CG/CGEXP1.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<graphics.h>
+void main()
+{
+ int gdriver=DETECT,gmode;
+ float x1,x2,y1,y2;
+ float xn,yn,dx,dy;
+ float xi,yi,steps,i;
+
+ clrscr();
+
+ initgraph(&gdriver,&gmode,"C:\\TC\\BGI");
+ printf("Domingos J Pinto\nSE CMPN B \nRoll No 16 \n192093\n");
+ printf("Note to user:500>x>25 500>y>200\n");
+ printf("Enter value of starting point (x1,y1) :\n");
+ scanf("%f%f",&x1,&y1);
+ printf("Enter value of starting point (x2,y2) :\n");
+ scanf("%f%f",&x2,&y2);
+ xn=x1;
+ yn=y1;
+ dx=x2-x1;
+ dy=y2-y1;
+ if(dx>=dy)
+ {
+  steps=abs(dx);
+  }
+ else
+ {
+  steps=abs(dy);
+  }
+ xi=dx/steps;
+ yi=dy/steps;
+ for(i=0;i<=steps;i++)
+ {
+  putpixel(xn,yn,RED);
+  xn=xn+xi;
+  yn=yn+yi;
+  }
+ getch();
+}
+

3106383645ddba9b28034bd290156b7248c066f3
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-09-08 16:35:31 +0530

    Single Linked List

--- SFIT/SE/DS/Learning/LINKLIST.C
+++ SFIT/SE/DS/Learning/LINKLIST.C
@@ -9,11 +9,18 @@ typedef struct node
 }node;
 
 void display(node*);
+node * create_list(node *);
 node * insert_beg(node *);
 node * insert_end(node *);
 node * insert_at(node *);
 node * insert_before(node *);
 node * insert_after(node *);
+node * delete_beg(node *);
+node * delete_end(node *);
+node * delete_at(node *);
+node * delete_before(node *);
+node * delete_after(node *);
+node * delete_value(node *);
 int main()
 {
 	struct node * start=NULL;
@@ -21,25 +28,42 @@ int main()
 	clrscr();
 	while(1)
 	{
-		printf("\nMenu:\n1.Insert_beg\n2.Insert_end\n3.Insert_at");
-		printf("\n4.Insert_before\n5.Insert_after\n6.Display\n7.Exit");
+		printf("\nMenu:\n1.Create list\t2.Insert_beg\n3.Insert_end");
+		printf("\t4.Insert_at\n5.Insert_before\t6.Insert_after");
+		printf("\n7.Delete_beg\t8.Delete_end\n9.Delete_at");
+		printf("\t10.Delete_before\n11.Delete_after\t12.Delete_value");
+		printf("\n13.Display\t14.Exit");
 		printf("\nEnter your choice: ");
 		scanf("%d",&ch);
 		switch(ch)
 		{
-			case 1: start=insert_beg(start);
+			case 1: start=create_list(start);
 				break;
-			case 2: start=insert_end(start);
+			case 2: start=insert_beg(start);
 				break;
-			case 3: start=insert_at(start);
+			case 3: start=insert_end(start);
 				break;
-			case 4: start=insert_before(start);
+			case 4: start=insert_at(start);
 				break;
-			case 5: start=insert_after(start);
+			case 5: start=insert_before(start);
 				break;
-			case 6: display(start);
+			case 6: start=insert_after(start);
 				break;
-			case 7: exit(0);
+			case 7: start=delete_beg(start);
+				break;
+			case 8: start=delete_end(start);
+				break;
+			case 9: start=delete_at(start);
+				break;
+			case 10: start=delete_before(start);
+				break;
+			case 11: start=delete_after(start);
+				break;
+			case 12: start=delete_value(start);
+				break;
+			case 13: display(start);
+				break;
+			case 14: exit(0);
 			default : printf("You entered wrong choice");
 		}
 	}
@@ -209,4 +233,207 @@ node * insert_at(node *start)
 		}
 	}
 	return start;
+}
+
+node * delete_beg(node * start)
+{
+	node * ptr;
+	if(start==NULL)
+		printf("List empty , Can't delete");
+	else
+	{
+		ptr=start;
+		start=start->next;
+		free(ptr);
+	}
+	return start;
+}
+
+node * delete_end(node * start)
+{
+	node * ptr;
+	if(start==NULL)
+		printf("List empty , Can't delete");
+	else
+	{
+		ptr=start;
+		while(ptr->next->next!=NULL)
+		{
+			ptr=ptr->next;
+		}
+		free(ptr->next);
+		ptr->next=NULL;
+	}
+	return start;
+}
+
+node * delete_at(node *start)
+{
+	node * ptr1,*ptr2;
+	int pos,count=1;
+	printf("Enter position from which to delete : ");
+	scanf("%d",&pos);
+	if(start==NULL)
+		printf("List empty , Can't delete");
+	else if(pos==1)
+	{
+		ptr1=start;
+		start=start->next;
+		free(ptr1);
+	}
+	else
+	{
+		ptr1=start;
+		ptr2=start;
+		while(ptr2!=NULL&&count<pos)
+		{
+			count=count++;
+			ptr1=ptr2;
+			ptr2=ptr2->next;
+		}
+		if(count==pos)
+		{
+			ptr1->next=ptr2->next;
+			free(ptr2);
+		}
+		else
+		{
+			printf("Position doesn't exist, can't delete");
+		}
+	}
+	return start;
+}
+
+node * delete_before(node *start)
+{
+	node *ptr1,*ptr2;
+	int val;
+	printf("Enter value of node before which to delete : ");
+	scanf("%d",&val);
+	if(start==NULL)
+		printf("\nList empty , Can't delete\n");
+	else
+	{
+		ptr1=start;
+		ptr2=start->next;
+		if(ptr1->data==val)
+		{
+			printf("\nThis is the first node. Nothing to delete\n");
+			return start;
+		}
+		if(ptr2->data==val)
+		{
+			ptr1=start;
+			start=start->next;
+			free(ptr1);
+			return start;
+		}
+		while(ptr2->next!=NULL&&ptr2->next->data!=val)
+		{
+			ptr1=ptr2;
+			ptr2=ptr2->next;
+		}
+		if(ptr2->next==NULL)
+		{
+			printf("\nValue not found, Can't delete\n");
+		}
+		else
+		{
+			ptr1->next=ptr2->next;
+			free(ptr2);
+		}
+	}
+	return start;
+}
+
+node * delete_after(node *start)
+{
+    if (start==NULL)
+    {
+	printf("\nList empty!\n");
+    }
+    else
+    {
+	node *ptr1,*ptr2;
+	int val;
+
+	printf("Enter the node value after which you want to delete node: ");
+	scanf("%d",&val);
+
+	ptr1=start;
+	ptr2=start->next;
+
+	while (ptr2->next!=NULL&&ptr1->data!=val)
+	{
+	    ptr1=ptr2;
+	    ptr2=ptr2->next;
+	}
+	if(ptr2->next==NULL)
+		{
+			printf("\nValue not found, Can't delete\n");
+		}
+	else
+	{
+		ptr1->next=ptr2->next;
+	}
+	free(ptr2);
+    }
+    return start;
+};
+
+node * delete_value(node *start)
+{
+	node *ptr1,*ptr2;
+	int val;
+	printf("Enter value of node to delete : ");
+	scanf("%d",&val);
+	if(start==NULL)
+		printf("\nList empty , Can't delete\n");
+	else
+	{
+		ptr1=start;
+		ptr2=start->next;
+		if(ptr1->data==val)
+		{
+			ptr1=start;
+			start=start->next;
+			free(ptr1);
+			return start;
+		}
+		while(ptr2->next!=NULL&&ptr2->data!=val)
+		{
+			ptr1=ptr2;
+			ptr2=ptr2->next;
+		}
+		if(ptr2->next==NULL)
+		{
+			printf("\nValue not found, Can't delete\n");
+		}
+		else
+		{
+			ptr1->next=ptr2->next;
+			free(ptr2);
+		}
+	}
+	return start;
+}
+
+node * create_list(node * start)
+{
+	if (start==NULL)
+	{
+		int n,i;
+		clrscr();
+		printf("Enter number of nodes : ");
+		scanf("%d",&n);
+		for(i=0;i<n;i++)
+		{
+			start=insert_end(start);
+		}
+	}
+	else
+    {
+        printf("\nList already exists!\n");
+    }
+    return start;
 }

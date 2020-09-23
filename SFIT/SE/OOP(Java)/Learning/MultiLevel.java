6a276759090bda88626b761552114135cc281b6f
Domingos J Pinto <67871136+newdjp2001@users.noreply.github.com>
2020-09-23 17:15:53 +0530

    Multilevel Inheritance v2

--- SFIT/SE/OOP(Java)/Learning/MultiLevel.java
+++ SFIT/SE/OOP(Java)/Learning/MultiLevel.java
@@ -1,20 +1,38 @@
 class Animal{
+	int limbs;
+	Animal(int limbs)
+	{
+		this.limbs=limbs;
+	}
 	void cellular()
 	{
 		System.out.println("They are multicellular organism");
+		System.out.println("They have "+limbs+" limbs");
 	}
 }
 
 class Mammals extends Animal{
+	int eyes;
+	Mammals(int limbs,int eyes){
+		super(limbs);
+		this.eyes=eyes;
+	}
 	void mammalia()
-	{
+	{	
 		System.out.println("They have mammary glands and hair");
+		System.out.println("They have "+eyes+" eyes");
 	}
 }
 
 class Dog extends Mammals{
+	int tail;	
+	Dog(int limbs,int eyes,int tail){
+		super(limbs,eyes);
+		this.tail=tail;
+	}
 	void character(){
 		System.out.println("They are loyal and bark.");
+		System.out.println("They have "+tail+" tail which they wag when they are happy");
 	}
 }
 
@@ -22,7 +40,7 @@ public class MultiLevel {
 
 	public static void main(String[] args) {
 		System.out.println("Characteristics of a Dog");
-		Dog dog = new Dog();
+		Dog dog = new Dog(4,2,1);
 		dog.cellular();
 		dog.mammalia();
 		dog.character();
